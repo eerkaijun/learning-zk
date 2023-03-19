f341653be1d5c769b6ba1b50c29675e2cf01a29f
eerkaijun <kaijuneer@gmail.com>
2023-03-19 18:46:21 +0000

    generate proof - not working

--- app/src/app.js
+++ app/src/app.js
@@ -1,14 +1,38 @@
-import { builder } from './witness_calculator'
-import { groth16 } from 'snarkjs'
+const builder = require('./witness_calculator');
+const snarkjs = require('snarkjs');
 
-const zkeyPath = 'authentication__prod.0.zkey'
-const wasmPath = 'authentication__prod.wasm'
+const zkeyPath = './zkeys/authentication__prod.0.zkey'
+const wasmPath = './zkeys/authentication__prod.wasm'
 
 const calculateProof = async (
     address,
-    secret,
+    secret
 ) => {
-    alert("Testing!")
+    // construct the circuit inputs
+    const circuitInputs = {
+        hashedSecret: BigInt('1121645852825515626345503741442177404306361956507933536148868635850297893661'),
+        address: BigInt(address),
+        secret: BigInt(secret),
+    }
+
+    // Fetch the zkey and wasm files, and convert them into array buffers
+    let resp = await fetch(wasmPath)
+    const wasmBuff = await resp.arrayBuffer()
+    resp = await fetch(zkeyPath)
+    const zkeyBuff = await resp.arrayBuffer()
+
+    console.log('wasmBuff', wasmBuff)
+    console.log('zkeyBuff', zkeyBuff)
+
+    // Calculate the witness
+    const witnessCalculator = await builder(wasmBuff)
+    const wtnsBuff = await witnessCalculator.calculateWTNSBin(circuitInputs, 0)
+    console.log('wtnsBuff', wtnsBuff)
+
+    const start = Date.now()
+    const { proof, publicSignals } =
+        await groth16.prove(new Uint8Array(zkeyBuff), wtnsBuff, null)
+    const end = Date.now()
 }
 
 const main = async () => {
