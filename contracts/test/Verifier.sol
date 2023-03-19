b93ec67ae45c852a885b8c5dd3c71ab2414312e2
eerkaijun <kaijuneer@gmail.com>
2023-03-19 21:12:04 +0000

    setup forge verifier

---
+++ contracts/test/Verifier.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import "../src/Verifier.sol";
+
+contract VerifierTest is Test {
+    Verifier public verifier;
+
+    function setUp() public {
+        // TODO: deploy verifier contract
+    }
+
+    function testVerification() public {
+
+    }
+}
